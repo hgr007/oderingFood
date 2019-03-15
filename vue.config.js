1f9ffbdcd2526b83e2af89ee12f9501c808a23bf
he <12343243@qq.com>
2019-03-15 20:31:52 +0800

    12578

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+module.exprots = {
+    baseUrl: '',            //配置根路径
+    outputDir: '',           //构建输出目录
+    assetDir: '',          //静态资源目录（js,css,img,fonts）
+    lintOnSave: false,           // 是否开启eslint 保存检测 ，有效值：true || false || ‘error’
+    devServe: {
+        // open: true,
+        // host: 'locahost',
+        // port: 8080,                       //设置打开的地址
+        // https: false,             //是否为HTTPS  可能会给与一些警告
+        proxy: {
+             //配置跨域
+            //  target:'http//locahost:5000/api,',        //请求后端接口
+            //  ws:true,                        //是否跨域
+            //  changeOrigin: true,         //
+            //  patRewrite:{                 //重写域名
+            //     '^/api':''
+            //  }
+
+        }
+    }
+};
