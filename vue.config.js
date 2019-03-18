e88d58fc5441118d039fb63a05da03d14c0ff12e
he <12343243@qq.com>
2019-03-18 14:54:54 +0800

    12

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    baseUrl: '',            //配置根路径
-    outputDir: '',           //构建输出目录
-    assetDir: '',          //静态资源目录（js,css,img,fonts）
     lintOnSave: false,           // 是否开启eslint 保存检测 ，有效值：true || false || ‘error’
-    devServe: {
+    // baseUrl: '',            //配置根路径
+    // outputDir: '',           //构建输出目录
+    // assetDir: '',          //静态资源目录（js,css,img,fonts）
+    // devServe: {
         // open: true,
         // host: 'locahost',
         // port: 8080,                       //设置打开的地址
         // https: false,             //是否为HTTPS  可能会给与一些警告
-        proxy: {
+        // proxy: {
              //配置跨域
             //  target:'http//locahost:5000/api,',        //请求后端接口
             //  ws:true,                        //是否跨域
@@ -17,6 +17,6 @@ module.exports = {
             //     '^/api':''
             //  }
 
-        }
-    }
+        // }
+    // }
 };
