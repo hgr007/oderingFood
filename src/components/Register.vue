6a95a752b0aa08548bd8c702408ffd094d8c9141
he <12343243@qq.com>
2019-03-18 19:26:23 +0800

    en

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,5 +1,42 @@
 <template>
-    <div>
-        <h1>这是注册页面</h1>
+  <div>
+    <div class="row mt-3">
+      <div class="col-md-12 col-lg-12">
+        <div class="card">
+          <div class="card-body">
+            <img class="mx-auto d-block" src="../assets/icon.png" alt="#">
+            <form @submit="Onsubmit">
+              <div class="form-group">
+                <label for="email">邮箱</label>
+                <input type="email" class="form-control" v-model="email">
+              </div>
+              <div class="form-group">
+                <label for="password">密码</label>
+                <input type="password" class="form-control" v-model="password">
+              </div>
+              <div class="form-group">
+                <label for="confirmPassword">确认密码</label>
+                <input type="confirmPassword" class="form-control" v-model="confirmPassword">
+              </div>
+              <button class="btn btn-block btn-success">注册</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      confirmPassword: ""
+    };
+  },
+  methods: {
+    Onsubmit() {}
+  }
+};
+</script>
